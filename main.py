5c7acc9d06a58b3ff1420fb36faca6fc0806d884
DripCharacter <wsms8646@gmail.com>
2021-06-19 03:31:14 +0900

    packet의 대략적인 활동 확인 완료(cacheList, rttList) 작동 좀 수정해야됨

--- main.py
+++ main.py
@@ -11,9 +11,15 @@ edgeData = pd.read_csv('topology_edge.csv')
 node = nodeData['node']
 xPos = nodeData['xPos']
 yPos = nodeData['yPos']
+nodeType=nodeData['nodeType']
+relatedCache=nodeData['relatedCache']
+relatedServer=nodeData['relatedServer']
 nodeList = node.values.tolist()
 xPosList = xPos.values.tolist()
 yPosList = yPos.values.tolist()
+nodeTypeList=nodeType.values.tolist()
+relatedCacheList=relatedCache.values.tolist()
+relatedServerList=relatedServer.values.tolist()
 edgeA = edgeData['nodeA']
 edgeB = edgeData['nodeB']
 edgeNum = edgeData['edgeNum']
@@ -23,7 +29,8 @@ edgeNumList = edgeNum.values.tolist()
 
 endedPacketSeries=0
 CONST_PACKETSERIES_LIMIT=100
-CONST_FACTOR=10
+CONST_FACTOR=0.1
+CONST_CACHE_SIZE=10
 
 G = nx.Graph()
 G.add_nodes_from(nodeList)
@@ -31,6 +38,26 @@ G.add_nodes_from(nodeList)
 for node in nodeList:
     G.nodes[node]['xPos'] = xPosList[nodeList.index(node)]
     G.nodes[node]['yPos'] = yPosList[nodeList.index(node)]
+
+cacheList=[]
+for i in nodeList:
+    eachCache=[]
+    for i in range(0, len(nodeList)):
+        if nodeTypeList[i-1]!=0:
+            eachCache.append(randint(1, 100))
+        else:
+            eachCache.append(0)
+    cacheList.append(eachCache)
+
+rttList=[]
+for i in nodeList:
+    eachRtt=[]
+    rttList.append(eachRtt)
+
+clientList=[]
+for i in nodeList:
+    if nodeTypeList[i-1]==0:
+        clientList.append(i)
 
 for edgeNum in edgeNumList:
     nodeA = G.nodes[edgeAList[edgeNum]]
@@ -43,12 +70,12 @@ for edgeNum in edgeNumList:
     G.add_edge(edgeAList[edgeNum], edgeBList[edgeNum], weight=edgeWeight)
 
 
-async def updateCache(G, dst, realdst, payload, cachelist):
+def updateCache(G, dst, realdst, payload, cachelist):
         global CONST_FACTOR
         linkWeight = G.edges[dst, realdst]['weight']
         sleepTime = linkWeight*CONST_FACTOR
-        await asyncio.sleep(sleepTime)
-        await asyncio.sleep(sleepTime)
+        time.sleep(sleepTime)
+        time.sleep(sleepTime)
         del cachelist[dst-1][0]
         cachelist[dst-1].append(payload)
 
@@ -58,6 +85,7 @@ async def packet(G, src, dst, realdst, payload, cachelist, rttList):
     global CONST_PACKETSERIES_LIMIT
     global CONST_FACTOR
     endedPacketSeries += 1
+    print(endedPacketSeries)
 
     linkWeight = G.edges[src, dst]['weight']
 
@@ -76,21 +104,35 @@ async def packet(G, src, dst, realdst, payload, cachelist, rttList):
     linkWeight = G.edges[src, realdst]['weight']
     sleepTime=linkWeight*CONST_FACTOR
 
+    updateStart=0
+    updateEnd=0
     if dataexistencebool:
         sleepTime=0
     else:
-        asyncio.run(updateCache(G, dst, realdst, payload, cachelist))
+        updateStart=time.time()
+        updateCache(G, dst, realdst, payload, cachelist)
+        updateEnd = time.time()
 
     await asyncio.sleep(sleepTime)
     await asyncio.sleep(sleepTime)
 
     end = time.time()
-    rttList.append(end - start)
+    rttList.append(end - start - (updateEnd-updateStart))
 
-    newPayload = randint(1, 100)
+async def main():
+    randIntList=[]
+    for node in nodeList:
+        payload=randint(1, 100)
+        randIntList.append(payload)
+    futures=[asyncio.ensure_future(packet(G, node, relatedCacheList[node-1], relatedServerList[node-1], randIntList[node-1], cacheList, rttList)) for node in clientList]
 
-    if endedPacketSeries<CONST_PACKETSERIES_LIMIT:
-        asyncio.run(packet(G, src, dst, realdst, newPayload, cachelist, rttList)) # 새로운 request니까 payload 바꿔서 보내야됨
+    result=await asyncio.gather(*futures)
+
+while endedPacketSeries<CONST_PACKETSERIES_LIMIT:
+    asyncio.run(main())
+
+print(cacheList)
+print(rttList)
 
 pos = nx.spring_layout(G)
 nx.draw(G, pos=pos, with_labels=True)
